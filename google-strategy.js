c46bad07f414b8be177a2ed4548af4619dfec954
Shourya Pal <palshourya897@gmail.com>
2024-04-10 18:07:08 +0530

    Setting Serialize and Deserialize

--- google-strategy.js
+++ google-strategy.js
@@ -43,10 +43,15 @@ module.exports = (passport) => {
   );
 
   passport.serializeUser((user, done) => {
-    done(null, user);
+    done(null, user.id);
   });
 
-  passport.deserializeUser((user, done) => {
-    done(null, user);
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
   });
 };
